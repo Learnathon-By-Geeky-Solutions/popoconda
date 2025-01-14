2e4885eee359290007100590db992db0067dec34
Ibnus Nahiyan Samit <nahiyansamit@gmail.com>
2025-01-14 19:29:59 +0600

    Added enemy fire laser functionality
    Added FireLaser script

--- Assets/Scripts/Characters/Enemy.cs
+++ Assets/Scripts/Characters/Enemy.cs
@@ -7,16 +7,27 @@ namespace Characters
         [SerializeField] protected GameObject player;
         [SerializeField] protected GameObject gun;
         
+        
         public Vector3 directionToPlayer;
         public float rotationZ;
-        // player distance
         public float distanceToPlayer;
         
         public void GetPlayerPosition()
         {
-            distanceToPlayer = Vector3.Distance(player.transform.position, gun.transform.position);
+            distanceToPlayer = Mathf.Abs(transform.position.x - player.transform.position.x);
             directionToPlayer = player.transform.position - gun.transform.position;
             rotationZ = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg;
+        }
+        
+        public void MoveTowardsPlayer()
+        {
+            
+            if (distanceToPlayer > 12f)
+            {
+                Vector3 targetPosition = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+                
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, 1f * Time.deltaTime);
+            }
         }
     }
 }
--- Assets/Scripts/Combat/ShootingController.cs
+++ Assets/Scripts/Combat/ShootingController.cs
@@ -7,134 +7,95 @@ namespace Combat
     {
         [SerializeField] private GameObject bulletSpawnPoint;
         [SerializeField] private TrailRenderer bulletTrail;
-        
-        // bullet force
-        [SerializeField] private float shootForce, upwardForce;
-        
-        // Gun Stats
-        [SerializeField] private float timeBetweenShooting, spread, reloadTime, timeBetweenShots;
+        [SerializeField] private float timeBetweenShooting, spread, reloadTime;
         [SerializeField] private int magazineSize, bulletsPerTap;
-        
-        // bool
-        private bool _isReadyToShoot, _isReloading;
-        
+        private bool _isReadyToShoot = true, _isReloading;
         private int _bulletsLeft, _bulletsShot;
-        
-        // points
-        private Vector3 _shootDirection;
-
-        private const float MinTravelDistance = 0.5f;
         [SerializeField] private int damageAmount = 10;
-        
+        private const float MinTravelDistance = 0.5f;
         
         private void Awake()
         {
             _bulletsLeft = magazineSize;
-            _isReadyToShoot = true;
         }
         
-        public void FireBullet(Vector3 direction, float rotationZ)
+        public void FireBullet(Vector3 direction)
         {
-            _shootDirection = direction;
-            
-            _bulletsShot = 0;
-            if (_bulletsLeft == 0)
+            if (_isReadyToShoot && _bulletsLeft > 0 && !_isReloading)
+            {
+                _bulletsShot = 0;
+                StartCoroutine(Shoot(direction));
+            }
+            else if (_bulletsLeft <= 0)
             {
                 Reload();
             }
-            
-            if (_isReadyToShoot && _bulletsLeft > 0 && !_isReloading)
-            {
-                Shoot();
-            }
         }
-        
-        private void Shoot()
+
+        private IEnumerator Shoot(Vector3 direction)
         {
             _isReadyToShoot = false;
-            _bulletsLeft--;
-            _bulletsShot++;
-    
-            // Spread calculation
-            float x = Random.Range(-spread, spread);
-            float y = Random.Range(-spread, spread);
-            Vector3 spreadVector = bulletSpawnPoint.transform.right * x + bulletSpawnPoint.transform.up * y;
-    
-            // Calculate shoot direction with spread
-            Vector3 shootDirection = (_shootDirection + spreadVector).normalized;
 
-            // Perform raycast
-            RaycastHit hit;
-            if (Physics.Raycast(bulletSpawnPoint.transform.position, shootDirection, out hit, Mathf.Infinity))
+            while (_bulletsShot < bulletsPerTap && _bulletsLeft > 0)
             {
-        
-                // Calculate the distance the bullet has traveled
-                float travelDistance = Vector3.Distance(bulletSpawnPoint.transform.position, hit.point);
-                
-                // instantiate bullet trail
-                TrailRenderer trail = Instantiate(bulletTrail, bulletSpawnPoint.transform.position, Quaternion.identity);
-                
-                // start a coroutine
-                StartCoroutine(SpawnTrail(trail,hit));
+                _bulletsLeft--;
+                _bulletsShot++;
 
-                // Check if the bullet has traveled at least the minimum distance before dealing damage
-                if (travelDistance >= MinTravelDistance)
+                // Apply bullet spread
+                float x = Random.Range(-spread, spread);
+                float y = Random.Range(-spread, spread);
+                Vector3 spreadVector = bulletSpawnPoint.transform.right * x + bulletSpawnPoint.transform.up * y;
+                Vector3 shootDirection = (direction + spreadVector).normalized;
+
+                // Perform raycast
+                if (Physics.Raycast(bulletSpawnPoint.transform.position, shootDirection, out RaycastHit hit, Mathf.Infinity))
                 {
-                    // Attempt to get the Health component from the collided object
-                    Health health = hit.collider.GetComponent<Health>();
-                    if (health)
+                    
+                    Debug.Log("Bullet Hit: " + hit.collider.name);
+                    float travelDistance = Vector3.Distance(bulletSpawnPoint.transform.position, hit.point);
+                    TrailRenderer trail = Instantiate(bulletTrail, bulletSpawnPoint.transform.position, Quaternion.identity);
+                    StartCoroutine(SpawnTrail(trail, hit));
+
+                    if (travelDistance >= MinTravelDistance)
                     {
-                        // Deal damage to the object with the Health component
-                        health.TakeDamage(damageAmount);
+                        Health health = hit.collider.GetComponent<Health>();
+                        health?.TakeDamage(damageAmount);
                     }
                 }
+
+                yield return new WaitForSeconds(timeBetweenShooting);
             }
 
-            // Check if we should continue shooting
-            if (_bulletsShot < bulletsPerTap && _bulletsLeft > 0)
-            {
-                Invoke(nameof(Shoot), timeBetweenShooting);
-            }
-            else
-            {
-                InvokeRepeating(nameof(ResetShot), timeBetweenShots, timeBetweenShooting);
-            }
+            _isReadyToShoot = true;
         }
-        
+
         private IEnumerator SpawnTrail(TrailRenderer trail, RaycastHit hit)
         {
             float time = 0;
             Vector3 startPos = bulletSpawnPoint.transform.position;
+
             while (time < 1)
             {
                 trail.transform.position = Vector3.Lerp(startPos, hit.point, time);
                 time += Time.deltaTime / trail.time;
-                
                 yield return null;
-
             }
 
             trail.transform.position = hit.point;
             Destroy(trail.gameObject);
         }
 
-        
-        private void ResetShot()
-        {
-            CancelInvoke(nameof(ResetShot));
-            _isReadyToShoot = true;
-        }
-        
         private void Reload()
         {
             if (_isReloading) return;
-            
+
             _isReloading = true;
-            Invoke(nameof(ReloadFinished), reloadTime);
+            StartCoroutine(ReloadFinished());
         }
-        
-        private void ReloadFinished()
+
+        private IEnumerator ReloadFinished()
         {
+            yield return new WaitForSeconds(reloadTime);
             _bulletsLeft = magazineSize;
             _bulletsShot = 0;
             _isReloading = false;
