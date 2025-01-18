1b6410af62144f9a352a4c16664ad5bea40d2e5b
Ibnus Nahiyan Samit <nahiyansamit@gmail.com>
2025-01-18 20:11:20 +0600

    Updated Scripts for event based approach
    Updated Scripts with
    - Event and Delegate
    - Raycast - prefab shooting

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -1,177 +1,128 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 using Combat;
-using UI;
+using Game;
 
 namespace Characters
 {
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private GameObject playerObject;
         private Rigidbody _playerRigidbody;
-        [SerializeField] private GameObject playerGfx;
-        [SerializeField] private GameObject gunRotatePoint;
-        
-        [SerializeField] private InputActionReference positionAction;
-        [SerializeField] private InputActionReference fireAction;
-        [SerializeField] private InputActionReference moveAction;
-        [SerializeField] private InputActionReference jumpAction;
+        [SerializeField] private InputManager inputManager;
         
         private Camera _playerCamera;
-        private Vector3 _mousePosition;
-        
-        [SerializeField] private float moveSpeed;
-        
-        [SerializeField] private float flySpeed;
-        [SerializeField] private float jetpackFuelMax;
-        private float _jetpackFuel;
-        [SerializeField] private float fuelConsumeRate;
-        [SerializeField] private float fuelFillRate;
-        private bool _isJumping;
+        [SerializeField] private Health playerHealth;
+        [SerializeField] private Player player;
         
         private ShootingController _shootingController;
+        private Vector3 _direction;
+        private float _rotationZ;
         private bool _isFiring;
         
-        [SerializeField] private HudHandler hudHandler;
+        // Create a delegate for player position
+        public delegate void PlayerPosition(Vector3 position);
+        public static event PlayerPosition OnPlayerPosition;
         
-        private Health _playerHealth;
-        public float MaxHealth =>_playerHealth.maxHealth;
-        public float CurrentHealth =>_playerHealth.currentHealth;
+        public static event Health.StatEventWithFloat OnPlayerHealthChange;
+        public static event Health.StatEventWithFloat OnJetpackFuelChange;
         
-        void Awake()
+        private void Awake()
         {
             _playerCamera = Camera.main;
-            _playerRigidbody = playerObject.GetComponent<Rigidbody>();
-            _playerHealth = playerObject.GetComponent<Health>();
-            _shootingController = playerObject.GetComponent<ShootingController>();
-            
-            
-            _jetpackFuel = jetpackFuelMax;
+            _playerRigidbody = gameObject.GetComponent<Rigidbody>();
+            _shootingController = gameObject.GetComponent<ShootingController>();
+            player.Initialize();
+            playerHealth.Initialize();
+            playerHealth.OnHealthChange += UpdateHealthUI;
+            playerHealth.OnDeath += OnPlayerDeath;
+            Bullet.OnBulletHit += ApplyDamage;
+            FireLaser.OnLaserHit += ApplyDamage;
         }
 
-        void Update()
+        private void Update()
         {
-            if (_isJumping)
-            {
-                HandleJetpack();
-            }
-            else
-            {
-                if (_jetpackFuel < jetpackFuelMax)
-                {
-                    _jetpackFuel += Time.deltaTime * fuelFillRate;
-                    _jetpackFuel = Mathf.Clamp(_jetpackFuel, 0, jetpackFuelMax);
-                }
-            }
-            Aim();
-        }
-        void FixedUpdate()
-        {
-            HandleMovement();
-            if(_isFiring) HandleShooting();
-            hudHandler.UpdateJetpackFuel(_jetpackFuel, jetpackFuelMax);
-        }
-        
-        
-        private void HandleMovement()
-        {
-            float xInput = moveAction.action.ReadValue<float>();
-            _playerRigidbody.AddRelativeForce(Vector3.right * (xInput * moveSpeed * Time.fixedDeltaTime));
+            OnPlayerPosition?.Invoke(transform.position);
         }
 
-        private void HandleJetpack()
+        private static void UpdateHealthUI(float currentHealth)
         {
-            if (_jetpackFuel > 0)
-            {
-                _playerRigidbody.AddForce(Vector3.up * (flySpeed * Time.deltaTime), ForceMode.VelocityChange);
-                _jetpackFuel -= Time.deltaTime * fuelConsumeRate;
-            }
+            OnPlayerHealthChange?.Invoke(currentHealth);
         }
-        
-        private Vector3 _difference;
-        private float _rotationZ;
 
-        private void Aim()
+        private void OnPlayerDeath()
         {
-            
-            if(_playerCamera == null) return;
-            Vector2 screenPosition = positionAction.action.ReadValue<Vector2>();
-            Ray ray = _playerCamera.ScreenPointToRay(screenPosition);
-
-            Plane gunPlane = new Plane(Vector3.forward, gunRotatePoint.transform.position);
-
-            if (gunPlane.Raycast(ray, out float distance))
-            {
-                _mousePosition = ray.GetPoint(distance);
-            }
-
-            _difference = _mousePosition - gunRotatePoint.transform.position;
-            _rotationZ = Mathf.Atan2(_difference.y, _difference.x) * Mathf.Rad2Deg;
-
-            gunRotatePoint.transform.rotation = Quaternion.Euler(0f, 0f, _rotationZ);
-            FlipPlayerGraphics(_rotationZ);
-        }
-        
-        private void FlipPlayerGraphics(float rotation)
-        {
-            if (rotation > 90 || rotation < -90)
-            {
-                playerGfx.transform.localScale = new Vector3(-1, 1, 1);
-            }
-            else
-            {
-                playerGfx.transform.localScale = new Vector3(1, 1, 1);
-            }
-        }
-        
-        private void HandleShooting()
-        {
-            _shootingController.FireBullet(_difference.normalized);
+            player.Die();
         }
         
         private void OnEnable()
         {
-            moveAction.action.Enable();
-            jumpAction.action.Enable();
-            fireAction.action.Enable();
-
-            jumpAction.action.started += OnJumpStart;
-            jumpAction.action.canceled += OnJumpEnd;
-            
-            fireAction.action.performed += OnFireStart;
-            fireAction.action.canceled += OnFireEnd;
+            inputManager.OnPositionChanged += HandleMousePosition;
+            inputManager.OnMoveAxisChanged += HandleMoveAxis;
+            inputManager.OnJumpPressed += HandleJump;
+            inputManager.OnFirePressed += HandleFire;
         }
 
         private void OnDisable()
         {
-            moveAction.action.Disable();
-            jumpAction.action.Disable();
-            fireAction.action.Disable();
+            inputManager.OnPositionChanged -= HandleMousePosition;
+            inputManager.OnMoveAxisChanged -= HandleMoveAxis;
+            inputManager.OnJumpPressed -= HandleJump;
+            inputManager.OnFirePressed -= HandleFire;
+        }
 
-            jumpAction.action.started -= OnJumpStart;
-            jumpAction.action.canceled -= OnJumpEnd;
+        private void HandleMousePosition(Vector2 screenPosition)
+        {
+            if (_playerCamera == null && !player.IsAlive) return;
             
-            fireAction.action.performed -= OnFireStart;
-            fireAction.action.canceled -= OnFireEnd;
-        }
+            Ray ray = _playerCamera.ScreenPointToRay(screenPosition);
+            
+            Plane gunPlane = new Plane(Vector3.forward, player.GunRotatePoint.transform.position);
 
-        private enum InputActionState { Start, End }
+            // If the ray intersects the plane
+            if (gunPlane.Raycast(ray, out float distance))
+            {
+                Vector3 targetPosition = ray.GetPoint(distance);                    // Get the intersection point in world space
+                
+                _direction = targetPosition - player.GunRotatePoint.transform.position;
 
-        private void HandleJump(InputActionState state)
-        {
-            _isJumping = (state == InputActionState.Start);
-        }
+                _rotationZ = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;  // Calculate the rotation in degrees
 
-        private void HandleFire(InputActionState state)
-        {
-            _isFiring = (state == InputActionState.Start);
+                player.GunRotatePoint.transform.rotation = Quaternion.Euler(0f, 0f, _rotationZ);    // Rotate the gun towards the target
+                
+                player.PlayerGfx.transform.localScale = _direction.x < 0 ? new Vector3(-1, 1, 1) : new Vector3(1, 1, 1); // Flip the player sprite if needed
+                
+            }
         }
         
-        private void OnJumpStart(InputAction.CallbackContext context) => HandleJump(InputActionState.Start);
-        private void OnJumpEnd(InputAction.CallbackContext context) => HandleJump(InputActionState.End);
-        private void OnFireStart(InputAction.CallbackContext context) => HandleFire(InputActionState.Start);
-        private void OnFireEnd(InputAction.CallbackContext context) => HandleFire(InputActionState.End);
+        private void HandleMoveAxis(float value)
+        {
+            if(!player.IsAlive) return;
+            _playerRigidbody.AddRelativeForce(Vector3.right * (value * player.MoveSpeed * Time.fixedDeltaTime));
+        }
 
+        private void HandleJump()
+        {
+            if (!player.IsAlive) return;
+            if (player.JetpackFuel > 0)
+            {
+                _playerRigidbody.AddForce(Vector3.up * (player.FlySpeed * Time.deltaTime), ForceMode.VelocityChange);
+                player.JetpackFuel -= Time.deltaTime * player.FuelConsumeRate;
+                OnJetpackFuelChange?.Invoke(player.JetpackFuel/player.JetpackFuelMax);
+            }
+            OnJetpackFuelChange?.Invoke(player.JetpackFuel/player.JetpackFuelMax);
+        }
 
+        private void HandleFire()
+        {
+            if (!player.IsAlive) return;
+            _shootingController.FireBullet(_direction);
+        }
+        
+        private void ApplyDamage(int damage, GameObject hitObject)
+        {
+            if (hitObject == gameObject)
+            {
+                playerHealth.TakeDamage(damage);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Combat/Bullet.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+namespace Combat
+{
+    public class Bullet : MonoBehaviour
+    {
+        private float _speed;
+        private Vector3 _direction;
+        private Rigidbody _rigidbody;
+        private Vector3 _spawnPosition;
+        private int _damageAmount;
+        
+        public delegate void HitHandler(int damage, GameObject hitObject);
+        public static event HitHandler OnBulletHit;
+        
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _spawnPosition = transform.position;
+            Destroy(gameObject, 3f);
+        }
+        
+        public void SetSpeed(float bulletSpeed)
+        {
+            _speed = bulletSpeed;
+        }
+        
+        public void SetDirection(Vector3 bulletDirection)
+        {
+            _direction = bulletDirection;
+        }
+        
+        public void SetDamageAmount(int damage)
+        {
+            _damageAmount = damage;
+        }
+        
+        private void FixedUpdate()
+        {
+            _rigidbody.linearVelocity = _direction * _speed;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            float distance = Vector3.Distance(_spawnPosition, transform.position);
+            if (distance <= 12f)
+            {
+                OnBulletHit?.Invoke(_damageAmount * 2, other.gameObject);
+            }
+            else
+            {
+                OnBulletHit?.Invoke(_damageAmount, other.gameObject);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
