9dbcf829127024dd9f7f09b77eae6f069002cf18
Ibnus Nahiyan Samit <nahiyansamit@gmail.com>
2025-02-10 13:38:56 +0600

    updated input manager with lambda event

--- Assets/Scripts/Game/InputManager.cs
+++ Assets/Scripts/Game/InputManager.cs
@@ -2,48 +2,56 @@ using UnityEngine;
 using Cysharp.Threading.Tasks;
 using UnityEngine.InputSystem;
 using System.Threading;
+using System;
 
 namespace Game
 {
     public class InputManager : MonoBehaviour
     {
-        private static InputManager _instance;
-        
+        private static InputManager Instance { get; set; }
+
         [SerializeField] private InputActionReference positionAction;
         [SerializeField] private InputActionReference fireAction;
         [SerializeField] private InputActionReference moveAction;
         [SerializeField] private InputActionReference jumpAction;
         [SerializeField] private InputActionReference menuAction;
         [SerializeField] private InputActionReference cancelAction;
-        
+
         public delegate void PositionChangeDelegate(Vector2 position);
         public delegate void MoveAxisDelegate(float value);
         public delegate void SimpleActionDelegate();
-        
+
         public static event PositionChangeDelegate OnMousePositionChanged;
         public static event MoveAxisDelegate OnMoveAxisChanged;
         public static event SimpleActionDelegate OnJumpPressed;
         public static event SimpleActionDelegate OnFirePressed;
         public static event SimpleActionDelegate OnMenuPressed;
-        
         public static event SimpleActionDelegate OnCancelPressed;
 
         private CancellationTokenSource _moveCts;
         private CancellationTokenSource _jumpCts;
         private CancellationTokenSource _fireCts;
-        
+
+        private Action<InputAction.CallbackContext> _movePerformed;
+        private Action<InputAction.CallbackContext> _moveCanceled;
+        private Action<InputAction.CallbackContext> _jumpPerformed;
+        private Action<InputAction.CallbackContext> _jumpCanceled;
+        private Action<InputAction.CallbackContext> _firePerformed;
+        private Action<InputAction.CallbackContext> _fireCanceled;
+        private Action<InputAction.CallbackContext> _menuPerformed;
+        private Action<InputAction.CallbackContext> _cancelPerformed;
+
         private void Awake()
         {
-            if (_instance != null && _instance != this)
+            if (Instance != null && Instance != this)
             {
                 Destroy(gameObject);
                 return;
             }
 
-            _instance = this;
+            Instance = this;
             DontDestroyOnLoad(gameObject);
         }
-
 
         private void OnEnable()
         {
@@ -53,38 +61,57 @@ namespace Game
             fireAction.action.Enable();
             menuAction.action.Enable();
             cancelAction.action.Enable();
-            
+
             positionAction.action.performed += HandlePositionChange;
-            moveAction.action.performed += HandleMovePressed;
-            moveAction.action.canceled += HandleMoveReleased;
-            jumpAction.action.performed += HandleJumpPressed;
-            jumpAction.action.canceled += HandleJumpReleased;
-            fireAction.action.performed += HandleFirePressed;
-            fireAction.action.canceled += HandleFireReleased;
-            menuAction.action.performed += HandleMenuPressed;
-            cancelAction.action.performed += HandleCancelPressed;
+
+            _movePerformed = _ => HandleMovePressed();
+            _moveCanceled = _ => HandleMoveReleased();
+            _jumpPerformed = _ => HandleJumpPressed();
+            _jumpCanceled = _ => HandleJumpReleased();
+            _firePerformed = _ => HandleFirePressed();
+            _fireCanceled = _ => HandleFireReleased();
+            _menuPerformed = _ => HandleMenuPressed();
+            _cancelPerformed = _ => HandleCancelPressed();
+
+            moveAction.action.performed += _movePerformed;
+            moveAction.action.canceled += _moveCanceled;
+            jumpAction.action.performed += _jumpPerformed;
+            jumpAction.action.canceled += _jumpCanceled;
+            fireAction.action.performed += _firePerformed;
+            fireAction.action.canceled += _fireCanceled;
+            menuAction.action.performed += _menuPerformed;
+            cancelAction.action.performed += _cancelPerformed;
         }
 
         private void OnDisable()
         {
             positionAction.action.performed -= HandlePositionChange;
-            moveAction.action.performed -= HandleMovePressed;
-            moveAction.action.canceled -= HandleMoveReleased;
-            jumpAction.action.performed -= HandleJumpPressed;
-            jumpAction.action.canceled -= HandleJumpReleased;
-            fireAction.action.performed -= HandleFirePressed;
-            fireAction.action.canceled -= HandleFireReleased;
-            menuAction.action.performed -= HandleMenuPressed;
-            cancelAction.action.performed -= HandleCancelPressed;
             
+            moveAction.action.performed -= _movePerformed;
+            moveAction.action.canceled -= _moveCanceled;
+            jumpAction.action.performed -= _jumpPerformed;
+            jumpAction.action.canceled -= _jumpCanceled;
+            fireAction.action.performed -= _firePerformed;
+            fireAction.action.canceled -= _fireCanceled;
+            menuAction.action.performed -= _menuPerformed;
+            cancelAction.action.performed -= _cancelPerformed;
+
             positionAction.action.Disable();
             moveAction.action.Disable();
             jumpAction.action.Disable();
             fireAction.action.Disable();
             menuAction.action.Disable();
             cancelAction.action.Disable();
-            
-            // Cancel all the CancellationTokenSources
+
+            // Properly clean up events
+            OnMousePositionChanged = null;
+            OnMoveAxisChanged = null;
+            OnJumpPressed = null;
+            OnFirePressed = null;
+            OnMenuPressed = null;
+            OnCancelPressed = null;
+
+            // Dispose of all CancellationTokens
             _moveCts?.Cancel();
             _moveCts?.Dispose();
             _jumpCts?.Cancel();
@@ -92,68 +119,72 @@ namespace Game
             _fireCts?.Cancel();
             _fireCts?.Dispose();
         }
-        
+
         private static void HandlePositionChange(InputAction.CallbackContext context)
         {
             OnMousePositionChanged?.Invoke(context.ReadValue<Vector2>());
         }
 
-        private void HandleMovePressed(InputAction.CallbackContext context)
+        private void HandleMovePressed()
         {
+            _moveCts?.Cancel();
+            _moveCts?.Dispose();
             _moveCts = new CancellationTokenSource();
             MoveAction(_moveCts.Token).Forget();
         }
-        
-        private void HandleMoveReleased(InputAction.CallbackContext context)
+
+        private void HandleMoveReleased()
         {
             _moveCts?.Cancel();
         }
-        
-        private void HandleJumpPressed(InputAction.CallbackContext _)
+
+        private void HandleJumpPressed()
         {
+            _jumpCts?.Cancel();
+            _jumpCts?.Dispose();
             _jumpCts = new CancellationTokenSource();
             JumpAction(_jumpCts.Token).Forget();
         }
-        
-        private void HandleJumpReleased(InputAction.CallbackContext _)
+
+        private void HandleJumpReleased()
         {
             _jumpCts?.Cancel();
         }
 
-        private void HandleFirePressed(InputAction.CallbackContext _)
+        private void HandleFirePressed()
         {
+            _fireCts?.Cancel();
+            _fireCts?.Dispose();
             _fireCts = new CancellationTokenSource();
             FireAction(_fireCts.Token).Forget();
         }
-        
-        private void HandleFireReleased(InputAction.CallbackContext _)
+
+        private void HandleFireReleased()
         {
             _fireCts?.Cancel();
         }
-        
-        private static void HandleMenuPressed(InputAction.CallbackContext _)
+
+        private static void HandleMenuPressed()
         {
             Debug.Log("Menu pressed");
             OnMenuPressed?.Invoke();
         }
-        
-        private static void HandleCancelPressed(InputAction.CallbackContext _)
+
+        private static void HandleCancelPressed()
         {
             Debug.Log("Cancel pressed");
             OnCancelPressed?.Invoke();
         }
-        
+
         private async UniTaskVoid MoveAction(CancellationToken token)
         {
             while (!token.IsCancellationRequested)
             {
-                float moveValue = moveAction.action.ReadValue<float>();
-                OnMoveAxisChanged?.Invoke(moveValue);
-                await UniTask.Yield(PlayerLoopTiming.FixedUpdate, token);
+                OnMoveAxisChanged?.Invoke(moveAction.action.ReadValue<float>());
+                await UniTask.Yield(PlayerLoopTiming.Update, token);
             }
         }
 
-        
         private static async UniTaskVoid JumpAction(CancellationToken token)
         {
             while (!token.IsCancellationRequested)
@@ -162,7 +193,7 @@ namespace Game
                 await UniTask.Yield(PlayerLoopTiming.Update, token);
             }
         }
-        
+
         private static async UniTaskVoid FireAction(CancellationToken token)
         {
             while (!token.IsCancellationRequested)
