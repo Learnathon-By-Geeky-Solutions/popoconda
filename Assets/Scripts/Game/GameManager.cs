62751c1a9d66cc8c23915d58255db346748eee11
Ibnus Nahiyan Samit <nahiyansamit@gmail.com>
2025-02-28 16:48:27 +0600

    added level 2 & 3 cutscene

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -56,6 +56,7 @@ namespace Game
         {
             if (_playerTransform == null)
             {
+                Debug.Log("Player Transform Set");
                 _playerTransform = playerTransform;
             }
         }
