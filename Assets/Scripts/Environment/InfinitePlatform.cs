b9cbc19b5d3c21eee3691368830d0c884d1c25fc
Ibnus Nahiyan Samit <nahiyansamit@gmail.com>
2025-04-27 19:33:38 +0600

    updated infinite platform

--- Assets/Scripts/Environment/InfinitePlatform.cs
+++ Assets/Scripts/Environment/InfinitePlatform.cs
@@ -1,6 +1,59 @@
 using UnityEngine;
+using Cysharp.Threading.Tasks;
 
-public class InfinitePlatform
+namespace Environment
 {
-    
+    public class InfinitePlatform : MonoBehaviour
+    {
+        [SerializeField] private GameObject platformObject1;
+        [SerializeField] private GameObject platformObject2;
+        [SerializeField] private GameObject platformObject3;
+        [SerializeField] private float moveSpeed = 2f;
+        [SerializeField] private float resetHeight = 20f;  // Height where platform will reset to
+        [SerializeField] private float bottomLimit = 10f;  // Y position after which platform resets
+
+        private GameObject[] platforms;
+
+        private void Start()
+        {
+            platforms = new[] { platformObject1, platformObject2, platformObject3 };
+            InfinitePlatformAsync().Forget();
+        }
+        
+        private async UniTaskVoid InfinitePlatformAsync()
+        {
+            while (true)
+            {
+                foreach (var platform in platforms)
+                {
+                    platform.transform.position -= new Vector3(0f, moveSpeed * Time.deltaTime, 0f);
+
+                    if (platform.transform.position.y <= bottomLimit)
+                    {
+                        float highestY = GetHighestPlatformY();
+                        platform.transform.position = new Vector3(
+                            platform.transform.position.x, 
+                            highestY + resetHeight, 
+                            platform.transform.position.z
+                        );
+                    }
+                }
+                
+                await UniTask.Yield(); // wait for next frame
+            }
+        }
+
+        private float GetHighestPlatformY()
+        {
+            float highest = float.MinValue;
+            foreach (var platform in platforms)
+            {
+                if (platform.transform.position.y > highest)
+                {
+                    highest = platform.transform.position.y;
+                }
+            }
+            return highest;
+        }
+    }
 }
